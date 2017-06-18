ca0d839ca74e43c8fe23f9bede81e22043c66691
钱鹏 <891366402@qq.com>
2017-06-18 15:49:10 +0800

    新增筛选条件为测试

--- public/js/common2.js
+++ public/js/common2.js
@@ -5,8 +5,9 @@
 // var token =  $.cookie("token")
 
 /***
- * 针对于左侧菜单栏的条选中Json
- * 目前针对测试通过
+ * 针对于左侧菜单栏的条选中返回Object
+ * 只针对于这个项目
+ * 后续需要改变成公用的
  */
 var getSearchValue = function () {
     var atc = [
@@ -42,6 +43,41 @@ var getSearchValue = function () {
     if($("#province").val() != "") {
         edge = {
             "edge" : new Array($("#province").val())
+        }
+    }
+
+    var manufacturetype = null;
+    if($("#manufacturetype").val() != "") {
+        manufacturetype = {
+            "": $("#manufacturetype").val()
+        }
+    }
+
+    var manufacture = null;
+    if($("#manufacture").val() != "") {
+        manufacture = {
+            "": $("#manufacture").val()
+        }
+    }
+
+    var dosage = null;
+    if($("#dosage").val() != "") {
+        dosage = {
+            "": $("#dosage").val()
+        }
+    }
+
+    var specification = null
+    if($("#specification").val()!= "") {
+        specification = {
+            "": $("#specification").val()
+        }
+    }
+
+    var package = null;
+    if($("#package").val() != "") {
+        package = {
+            "": $("#package").val()
         }
     }
 
