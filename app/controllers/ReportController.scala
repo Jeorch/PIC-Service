7d75dc88ec5064a29761c711b81d0efbefedbf6b
钱鹏 <891366402@qq.com>
2017-06-26 11:20:37 +0800

    冲突修改

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -63,10 +63,8 @@ class ReportController @Inject () (as_inject : ActorSystem, dbt : DBTrait, att :
 		import bmpattern.ResultMessage.lst_result
 		
 		MessageRoutes(msg_log(toJson(Map("method" -> toJson("reportparameter"))), jv)
-<<<<<<< Updated upstream
 			:: msg_AuthTokenParser(jv) :: msg_CheckTokenExpire(jv) :: msg_CheckProductLevelScope(jv)
 			:: msg_ReportGraph_One(jv) :: msg_ReportGraph_Five(jv) :: msg_ReportGraph_Six(jv) :: msg_ReportGraph_Eight(jv) :: msg_ReportGraph_Seven(jv)
-=======
 			:: msg_AuthTokenParser(jv) :: msg_CheckTokenExpire(jv)
 			:: msg_CheckProductLevelScope(jv) ::msg_ReportParameterSummary(jv)
 			:: msg_CommonResultMessage() :: Nil, None)(CommonModules(Some(Map("db" -> dbt, "att" -> att))))
@@ -81,6 +79,7 @@ class ReportController @Inject () (as_inject : ActorSystem, dbt : DBTrait, att :
 			:: msg_CheckProductLevelScope(jv) :: msg_ReportGraph_Four(jv)
 			:: msg_CommonResultMessage() :: Nil, None)(CommonModules(Some(Map("db" -> dbt, "att" -> att))))
 	})
+	
 	def chartOne = Action(request => requestArgsQuery().requestArgsV2(request) { jv =>
 		import bmpattern.LogMessage.common_log
 		import bmpattern.ResultMessage.lst_result
@@ -88,7 +87,6 @@ class ReportController @Inject () (as_inject : ActorSystem, dbt : DBTrait, att :
 		MessageRoutes(msg_log(toJson(Map("method" -> toJson("reportparameter"))), jv)
 			:: msg_AuthTokenParser(jv) :: msg_CheckTokenExpire(jv)
 			:: msg_CheckProductLevelScope(jv) ::msg_ReportChart_one(jv)
->>>>>>> Stashed changes
 			:: msg_CommonResultMessage() :: Nil, None)(CommonModules(Some(Map("db" -> dbt, "att" -> att))))
 	})
 	
