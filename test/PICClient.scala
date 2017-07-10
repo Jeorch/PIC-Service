054c8d08a8eb96423001b9e161af33aeca84ac47
Unknown <jeorch@foxmail.com>
2017-07-10 20:45:40 +0800

    搜索功能的Specs单元测试完成
    搜索功能的Specs单元测试完成

--- test/PICClient.scala
+++ test/PICClient.scala
@@ -1,5 +1,6 @@
 import javax.inject.Inject
 
+import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 import play.api.libs.ws.WSClient
 
@@ -17,7 +18,40 @@ class PICClient(ws: WSClient, baseUrl: String)(implicit ec: ExecutionContext) {
             .post(toJson(Map("user_name" -> name, "pwd" -> pwd)))
             .map { response =>
                 println(response.json)
-                (response.json \ "status").asOpt[String].get
+                (response.json \ "result").get.asOpt[Map[String,JsValue]].get.get("auth_token").get.asOpt[String].get
             }
+    }
+
+    def conditionSearchTest(token : String, condition : Map[String, JsValue], skip : Int, contains : String) : Future[Int] = {
+        ws.url(baseUrl + "/data/search")
+            .withHeaders("Accept" -> "application/json","Content-Type" -> "application/json")
+                .post(toJson(Map("token" -> toJson(token),"condition" -> toJson(condition),"skip" -> toJson(skip))))
+                .map{   response =>
+                    println(response.json)
+                    var result = 0
+                    val pages = (response.json \ "page").get.asOpt[List[Map[String,JsValue]]].get.head.get("TOTLE_PAGE").get.asOpt[Int].get
+                    if (pages > 0){
+                        if ((response.json \ "search_result").asOpt[List[Map[String,JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length>0){
+                            result = 1
+                        }
+                        if (result==0){
+                            for (i <- 1 to pages){
+                                ws.url(baseUrl + "/data/search")
+                                    .withHeaders("Accept" -> "application/json","Content-Type" -> "application/json")
+                                    .post(toJson(Map("token" -> toJson(token),"condition" -> toJson(condition),"skip" -> toJson(i))))
+                                    .map{   response_ =>
+                                        if ((response_.json \ "search_result").asOpt[List[Map[String,JsValue]]].get.filterNot(x => x.get("html").get.asOpt[String].get.contains(contains)).length>0){
+                                            result = 1
+                                        }
+                                    }
+                            }
+                        }
+                        result
+                    }else{
+                        println("--> No Results! <--")
+                        result = 1
+                        result
+                    }
+                }
     }
 }
--- test/PICTestSpec.scala
+++ test/PICTestSpec.scala
@@ -7,8 +7,8 @@ import play.api.test._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
-
 import org.specs2.mutable.Specification
+import play.api.libs.json.Json.toJson
 
 /**
   * Created by alfredyang on 07/07/2017.
@@ -16,17 +16,69 @@ import org.specs2.mutable.Specification
 class PICTestSpec extends Specification {
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    override def is = s2"""
-        This is a PIC specification to check the 'auth with password' string
+    var token = ""
 
-            The 'PIC ' auth functions should
-                login with user alfred and password 12345                     $e1
+    val user_name = "alfred"
+    val pwd = "12345"
+    val start_time = "201501"
+    val end_time = "201601"
+    val category = "他汀类"
+    val oral_name = "阿托伐他汀"
+    val product_name = "阿乐"
+    val edge = List("北京")
+    val manufacture_name = List("北京嘉林药业股份有限公司")
+
+    val condition3_21 = Map("date" -> toJson(Map("start" -> toJson(start_time),"end" -> toJson(end_time))),"category" -> toJson(category),"oral_name" -> toJson(oral_name))
+    val condition3_23 = Map("date" -> toJson(Map("start" -> toJson(start_time),"end" -> toJson(end_time))),"category" -> toJson(category),"product_name" -> toJson(product_name))
+    val condition3_25 = Map("date" -> toJson(Map("start" -> toJson(start_time),"end" -> toJson(end_time))),"category" -> toJson(category),"product_name" -> toJson(product_name),"edge" -> toJson(edge))
+    val condition3_27 = Map("date" -> toJson(Map("start" -> toJson(start_time),"end" -> toJson(end_time))),"category" -> toJson(category),"product_name" -> toJson(product_name),"edge" -> toJson(edge),"manufacture_name" -> toJson(manufacture_name))
+
+    val contains = "他汀类"
+
+    val skip = 1
+    val time_out = 120
+
+    override def is = s2"""
+        This is a PIC specification to check the 'conditionSearch' string
+
+            The 'PIC ' conditionSearch functions should
+                auth with password user_name:${user_name},pwd:${pwd}                        $authToken
+                search with condition=>category: ${category}, oral_name: ${oral_name}, date=>start: ${start_time}, end: ${end_time}                                 $testCase3_21
+                search with condition=>category: ${category}, product_name: ${product_name}, date=>start: ${start_time}, end: ${end_time}                           $testCase3_23
+                search with condition=>category: ${category}, product_name: ${product_name}, edge: ${edge.head}, date=>start: ${start_time}, end: ${end_time}       $testCase3_25
+                search with condition=>category: ${category}, product_name: ${product_name}, edge: ${edge.head}, manufacture_name: ${manufacture_name.head}, date=>start: ${start_time}, end: ${end_time}       $testCase3_27
                                                                               """
 
-    def e1 =
+    def authToken =
     WsTestClient.withClient { client =>
         val result = Await.result(
-            new PICClient(client, "http://127.0.0.1:9000").authWithPasswordTest("alfred", "12345"), 5.seconds)
-        result must_== "ok"
+            new PICClient(client, "http://127.0.0.1:9000").authWithPasswordTest(user_name, pwd), time_out.seconds)
+        token = result
+        result must_!= ""
+    }
+
+    def testCase3_21 =
+    WsTestClient.withClient { client =>
+        val result = Await.result(
+            new PICClient(client, "http://127.0.0.1:9000").conditionSearchTest(token, condition3_21, skip, contains), time_out.seconds)
+        result must_== 0
+    }
+    def testCase3_23 =
+    WsTestClient.withClient { client =>
+        val result = Await.result(
+            new PICClient(client, "http://127.0.0.1:9000").conditionSearchTest(token, condition3_23, skip, contains), time_out.seconds)
+        result must_== 0
+    }
+    def testCase3_25 =
+    WsTestClient.withClient { client =>
+        val result = Await.result(
+            new PICClient(client, "http://127.0.0.1:9000").conditionSearchTest(token, condition3_25, skip, contains), time_out.seconds)
+        result must_== 0
+    }
+    def testCase3_27 =
+    WsTestClient.withClient { client =>
+        val result = Await.result(
+            new PICClient(client, "http://127.0.0.1:9000").conditionSearchTest(token, condition3_27, skip, contains), time_out.seconds)
+        result must_== 0
     }
 }
